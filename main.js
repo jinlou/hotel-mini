38a6b606e3ffe71bc0152f8757795fd804f1a42a
金楼 <43943825+jinlou@users.noreply.github.com>
2022-07-12 10:54:46 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import api from "./api/api"
 Vue.prototype.$api = api
 
 Vue.prototype.appID = 'wx2b0f1488c6f3405f' // 小程序的原始ID
+// Vue.prototype.appID = 'wx073f509b5bd69ba1' // 小程序的原始ID
+Vue.prototype.$serviceIDs = ['chPTJc26Z9Rd28dWTbJsExmYj3QltR6uOb7lkJn-pP0']
 
 Vue.config.productionTip = false
 
